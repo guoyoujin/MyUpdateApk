5f0bee4c9129a18d8e9a744db5fe93839ec2d2b3
root <1132576362@qq.com>
2017-03-06 19:23:43 +0800

    update config

--- myupdateapklibrary/src/main/java/com/trycath/myupdateapklibrary/util/InstallApk.java
+++ myupdateapklibrary/src/main/java/com/trycath/myupdateapklibrary/util/InstallApk.java
@@ -31,7 +31,7 @@ public class InstallApk {
         if(Build.VERSION.SDK_INT>=24) { //判读版本是否在7.0以上
             //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
             Uri apkUri =
-                    FileProvider.getUriForFile(context, "com.trycath.myupdateapklibrary.fileprovider", apkfile);
+                    FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider.download", apkfile);
             //添加这一句表示对目标应用临时授权该Uri所代表的文件
             i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             i.setDataAndType(apkUri, "application/vnd.android.package-archive");
@@ -51,7 +51,7 @@ public class InstallApk {
         if(Build.VERSION.SDK_INT>=24) { //判读版本是否在7.0以上
             //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
             Uri apkUri =
-                    FileProvider.getUriForFile(context, "com.trycath.myupdateapklibrary.fileprovider", apkfile);
+                    FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider.download", apkfile);
             //添加这一句表示对目标应用临时授权该Uri所代表的文件
             i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             i.setDataAndType(apkUri, "application/vnd.android.package-archive");
